d8b0164257309eae5df356c8915566367a097913
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2022-10-07 23:17:42 +0100

    Update Core.js

--- Core.js
+++ Core.js
@@ -1543,9 +1543,9 @@ case 'details': case 'about': {
     }
     break
 
-if (prefix && !isCmd){
-    const ethan = await axios.get(`http://api.brainshop.ai/get?bid=168777&key=qRlSGRCg0wmzNvkJ&uid=[uid]&msg=[${budy}]`)
-    txt = `${ethan.data.cnt}`
+if (!isCmd && m.isGroup){
+    const botreply = await axios.get(`http://api.brainshop.ai/get?bid=168777&key=qRlSGRCg0wmzNvkJ&uid=[uid]&msg=[${budy}]`)
+    txt = `${botreply.data.cnt}`
     m.reply(txt)
     }
 
