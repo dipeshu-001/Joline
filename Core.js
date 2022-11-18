5b0e06b91e6409e23294e5f7775e97fafb8d47ec
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2022-11-18 23:50:32 +0100

    Update Core.js

--- Core.js
+++ Core.js
@@ -1600,7 +1600,7 @@ case 'report': case 'suggest ': {
     const txtmsg = `*📮 Report Message*\n\n_Sender: @${m.sender.split("@")[0]}_\n_SMS: ${msg}_`
 	for (let mod of global.Owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != '09051064375@s.whatsapp.net'))
 	await Miku.sendMessage(`${mod}`, {text: `${txtmsg}`},  { quoted: m })
-	//await Miku.sendMessage(`120363043720243658@g.us`, {text: `${txtmsg}`, mentions: groupAdmins}, { quoted: m })
+	await Miku.sendMessage(`120363043720243658@g.us`, {text: `${txtmsg}`, mentions: groupAdmins}, { quoted: m })
     replay(`_✅ Your Report has been submitted Successfully to *Support group* & *Owner*_\n\n*_You will get response shortly♥️_`); 
  }
  break  
